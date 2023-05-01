98eb260f2820e37522eea25843cc4e46ac392051
Rajaavignesh <rajavignesh6769@gmail.com>
2023-05-01 20:44:07 +0530

    changes

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql");
 const app = express();
 const dotenv = require("dotenv");
-
 dotenv.config({ path: "./.env" });
+const multer = require("multer");
+const upload = multer();
+const creditCardType = require("credit-card-type");
 
-// const Env= use()
+var cors = require("cors");
 
 // Use body-parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+const corsOptions = {
+  origin: "*",
+  methods: ["GET,HEAD,PUT,PATCH,POST,DELETE"],
+};
+app.use(cors(corsOptions));
 // Serve index.html on the root path
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -23,21 +31,21 @@ app.get("/", (req, res) => {
 
 //Establish the database connection
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "db_project",
-});
+// const db = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "db_project",
+// });
 
-db.connect(function (error) {
-  if (error) {
-    console.log(error);
-    console.log("Error Connecting to DB");
-  } else {
-    console.log("Successfully connected to DB");
-  }
-});
+// db.connect(function (error) {
+//   if (error) {
+//     console.log(error);
+//     console.log("Error Connecting to DB");
+//   } else {
+//     console.log("Successfully connected to DB");
+//   }
+// });
 
 //Establish the port
 app.listen(5000, function check(error) {
@@ -53,18 +61,25 @@ paypal.configure({
 });
 
 // Handle credit card payment request
-app.post("/process_payment", async (req, res) => {
+app.post("/process_payment", upload.array(), async (req, res) => {
 
+
+  console.log("card number", req.body.card_number);
+
+  const cardType = creditCardType(req.body.card_number);
+
+  console.log("Card type:", cardType[0].niceType);
 
   const cardData = {
-    type: req.body.card_type,
+    type: cardType[0].niceType,
     number: req.body.card_number,
     expire_month: req.body.expire_month,
     expire_year: req.body.expire_year,
     cvv2: req.body.cvv2,
   };
 
- 
+  console.log("cardData", cardData);
+
   const paymentDetails = {
     intent: "sale",
     payer: {
@@ -90,9 +105,14 @@ app.post("/process_payment", async (req, res) => {
       },
     ],
   };
-  
-   if (
-    cardData.type === "AMEX" &&
+
+  if (req.body.currency != "USD" && cardData.type == "American Express") {
+    // Return error message
+    res.status(400).send("AMEX is possible to use only for USD");
+  }
+
+  if (
+    cardData.type === "American Express" &&
     (req.body.currency === "USD" ||
       req.body.currency === "EUR" ||
       req.body.currency === "AUD")
@@ -117,7 +137,7 @@ app.post("/process_payment", async (req, res) => {
         },
       ],
     };
-    let data
+    let data;
     paypal.payment.create(payment, function (error, payment) {
       if (error) {
         console.error(error);
@@ -137,82 +157,74 @@ app.post("/process_payment", async (req, res) => {
           transaction_id: payment.id,
           status: payment.state,
           currency_code: req.body.currency,
-          redirect_url: redirectUrl
+          redirect_url: redirectUrl,
         };
-        
-        res.status(200).send({data: data});
+
+        res.status(200).send({ data: data });
       }
     });
-
-
-
-
-
   } else {
-    if (req.body.currency !== "USD" && cardData.type === "AMEX") {
-      // Return error message
-      res.status(400).send("AMEX is possible to use only for USD");
-    } else {
-      // Use Braintree
+    // if (req.body.currency !== "USD" && cardData.type === "AMEX") {
+    //   // Return error message
+    //   res.status(400).send("AMEX is possible to use only for USD");
+    // } else {
+    // Use Braintree
 
-      console.log("Payment processed using Braintree");
+    console.log("Payment processed using Braintree");
 
-      const gateway = new braintree.BraintreeGateway({
-        environment: braintree.Environment.Sandbox,
-        merchantId: process.env.merchantId,
-        publicKey: process.env.publicKey,
-        privateKey: process.env.privateKey,
-      });
-      let data
-      // create a transaction sale request
-      let transactionRequest = {
-        amount: req.body.amount,
-        customer: {
-          firstName: "John",
-          lastName: "Doe",
-        },
-        creditCard: {
-          number: req.body.card_number,
-          expirationDate: `${req.body.expire_month}/${req.body.expire_year}`,
-          cvv: req.body.cvv2,
-        },
-        options: {
-          submitForSettlement: true,
-        },
-      };
+    const gateway = new braintree.BraintreeGateway({
+      environment: braintree.Environment.Sandbox,
+      merchantId: process.env.merchantId,
+      publicKey: process.env.publicKey,
+      privateKey: process.env.privateKey,
+    });
+    let data;
+    // create a transaction sale request
+    let transactionRequest = {
+      amount: req.body.amount,
+      customer: {
+        firstName: "John",
+        lastName: "Doe",
+      },
+      creditCard: {
+        number: req.body.card_number,
+        expirationDate: `${req.body.expire_month}/${req.body.expire_year}`,
+        cvv: req.body.cvv2,
+      },
+      options: {
+        submitForSettlement: true,
+      },
+    };
 
-      // make the transaction sale request
+    console.log(transactionRequest);
 
-      await gateway.transaction.sale(
-        transactionRequest,
-        function (err, result) {
-          if (err) {
-            console.error(err);
-            res.status(500).send(err);
-          }
+    // make the transaction sale request
 
-          if (result.success) {
-            console.log("Transaction ID: " + result.transaction.id);
+    await gateway.transaction.sale(transactionRequest, function (err, result) {
+      if (err) {
+        console.error(err);
+        res.status(500).send(err);
+      }
 
-            data = {
-              transaction_id: result.transaction.id,
-              status: result.transaction.status,
-              currency_code: result.transaction.currencyIsoCode,
-              amount: result.transaction.amount,
-            };
-            res.status(200).send({ data: data });
-          } else {
-            console.log(result.message);
-          }
+      if (result.success) {
+        console.log("Transaction ID: " + result.transaction.id);
 
-          console.log(data);
-        }
-      );
-    }
+        data = {
+          transaction_id: result.transaction.id,
+          status: result.transaction.status,
+          currency_code: result.transaction.currencyIsoCode,
+          amount: result.transaction.amount,
+        };
+        res.status(200).send({ data: data });
+      } else {
+        console.log(result.message);
+      }
+
+      console.log(data);
+      res.status(200).send({ data: data });
+    });
+    // }
   }
-
-
-  
 });
 
 //test payment Paypal
@@ -252,13 +264,14 @@ app.post("/create-payment", function (req, res) {
         }
       }
       //  console.log("redirectUrl", redirectUrl)
-      res.redirect(redirectUrl);
+      res.status(200).send(redirectUrl);
+      // res.redirect(redirectUrl);
     }
   });
 });
 
 app.get("/success", (req, res) => {
-  var paymentId = "PAYID-MQ6CYMY9JM447478A2779232";
+  var paymentId = "PAYID-MRHAGFA2K5347380K051673L";
   var payerId = { payer_id: req.query.PayerID };
 
   paypal.payment.execute(paymentId, payerId, function (error, payment) {
